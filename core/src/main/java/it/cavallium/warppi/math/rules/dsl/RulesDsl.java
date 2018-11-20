fa2b9f20a85548c7db4f7b369e5a59e1c3106704
Riccardo Azzolini <riccardo4096@gmail.com>
2018-11-20 19:27:04 +0100

    Define a temporarily empty RulesDsl.makeRules method
    When complete, this method will execute the DSL front-end, and return
    the list of rules, if successful, otherwise report errors.

---
+++ core/src/main/java/it/cavallium/warppi/math/rules/dsl/RulesDsl.java
@@ -0,0 +1,11 @@
+package it.cavallium.warppi.math.rules.dsl;
+
+import it.cavallium.warppi.math.rules.Rule;
+
+import java.util.List;
+
+public class RulesDsl {
+	public static List<Rule> makeRules(final String source) {
+		return null;
+	}
+}
