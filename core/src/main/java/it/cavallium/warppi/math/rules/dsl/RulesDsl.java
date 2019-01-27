a92c3a32720583acf505fd95f698a59fc8c6597a
Riccardo Azzolini <riccardo4096@gmail.com>
2019-01-27 19:18:09 +0100

    Implement and test RulesDsl.makeRules

--- core/src/main/java/it/cavallium/warppi/math/rules/dsl/RulesDsl.java
+++ core/src/main/java/it/cavallium/warppi/math/rules/dsl/RulesDsl.java
@@ -1,11 +1,18 @@
 package it.cavallium.warppi.math.rules.dsl;
 
 import it.cavallium.warppi.math.rules.Rule;
+import it.cavallium.warppi.math.rules.dsl.frontend.Lexer;
+import it.cavallium.warppi.math.rules.dsl.frontend.Parser;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RulesDsl {
+	private RulesDsl() {}
+
 	public static List<Rule> makeRules(final String source) {
-		return null;
+		final Lexer lexer = new Lexer(source);
+		final Parser parser = new Parser(lexer.lex());
+		return Collections.unmodifiableList(parser.parse());
 	}
 }
